4dca4e41bc47ea2b1e0c782be8c86f4b737fcb8e
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2024-10-02 00:51:34 +0600

    added the two functionalities for all the 3 cards when the Donate Now button of the cards is clicked

--- js/donate.js
+++ js/donate.js
@@ -1,31 +1,78 @@
-console.log('donate js file added')
+// console.log('donate js file added')
 
+// Card-1
 document.getElementById('button-donate-now')
     .addEventListener('click', function(event){
         event.preventDefault();
-        console.log('donate button clicked');
 
-        // const donationAmount = document.getElementById('input-donation-amount').value;
-        // console.log(donationAmount);
+        const donationAmount = getInputFieldValueById('input-donation-amount');
+        console.log('value by Donate Now button click', donationAmount);
 
-        // const balance = document.getElementById('account-balance').innerText;
-        // console.log(balance);
+        const balance = getTextFieldValueById('account-balance');
+        console.log(' balance here',  balance);
 
-        // const donationAmountNumber = parseFloat(donationAmount);
-        // const balanceNumber = parseFloat(balance);
-        // const newBalance = balanceNumber - donationAmountNumber;
-        // console.log(newBalance);
-        
-        // document.getElementById('account-balance').innerText = newBalance;
+        const newBalance = balance - donationAmount;
+        // console.log('new balance:', newBalance);
 
-        // const currentDonation = document.getElementById('current-donation-amount').innerText;
-        // console.log(currentDonation);
+        document.getElementById('account-balance').innerText = newBalance;
 
-        // const currentDonationNumber = parseFloat(currentDonation);
-        // const newCurrentDonation = currentDonationNumber + donationAmountNumber;
-        // console.log(newCurrentDonation);
+        const currentDonation = getTextFieldValueById('current-donation-amount');
+        console.log('current donation here', currentDonation);
 
-        // document.getElementById('current-donation-amount').innerText = newCurrentDonation;
+        const newCurrentDonation = currentDonation + donationAmount;
+        // console.log('new current donation:', newCurrentDonation);
 
-        getInputFieldValueById();
-})
+        document.getElementById('current-donation-amount').innerText = newCurrentDonation;
+});
+
+// Card-2
+document.getElementById('button-donate-now-2')
+    .addEventListener('click', function(event){
+    event.preventDefault();
+
+    const donationAmount2 = getInputFieldValueById('input-donation-amount-2');
+    console.log(' value by Donate Now 2 button click', donationAmount2);
+
+    const balance = getTextFieldValueById('account-balance');
+    console.log('2nd time balance here', balance);
+
+    const newBalance2 = balance - donationAmount2;
+    // console.log('new balance2:', newBalance2);
+
+    document.getElementById('account-balance').innerText = newBalance2;
+
+    const currentDonation2 = getTextFieldValueById('current-donation-amount-2');
+    console.log('current donation2 here', currentDonation2);
+
+    const newCurrentDonation2 = currentDonation2 + donationAmount2;
+    // console.log('new current donation2', newCurrentDonation2);
+
+    document.getElementById('current-donation-amount-2').innerText = newCurrentDonation2;
+
+});
+
+// Card-3
+document.getElementById('button-donate-now-3')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        const donationAmount3 = getInputFieldValueById('input-donation-amount-3');
+        console.log('value by Donate now button3 click', donationAmount3);
+
+        const balance = getTextFieldValueById('account-balance');
+        console.log('3rd time balance here,', balance);
+
+        const newBalance3 = balance - donationAmount3;
+        // console.log('new balance3,', newBalance3);
+
+        document.getElementById('account-balance').innerText = newBalance3;
+
+        const currentDonation3 = getTextFieldValueById('current-donation-amount-3');
+        console.log('current donation amount3 here,', currentDonation3);
+
+        const newCurrentDonation3 = currentDonation3 + donationAmount3;
+        // console.log('new current donation3:', newCurrentDonation3);
+
+        document.getElementById('current-donation-amount-3').innerText = newCurrentDonation3;
+
+});
--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,22 @@
-console.log('utilities loaded');
+// console.log('utilities loaded');
 
-function getInputFieldValueById(){
-    console.log('get value by id');
+// function getInputFieldValueById(){
+    // console.log('get value by id');
+    // const donationAmount = document.getElementById('input-donation-amount').value;
+    // return donationAmount;
+// }
+
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    // return inputValue;
+    const inputNumber = parseFloat(inputValue);
+    // console.log(id, inputValue, inputNumber);
+    return inputNumber;
+}
+
+function getTextFieldValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    // console.log(id, textValue, textNumber);
+    return textNumber;
 }
